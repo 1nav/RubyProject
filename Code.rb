de8ae5ba1665bbfae3c4351f82c2f34a6a95ade4
1nav <nav.fess@gmail.com>
2018-01-11 00:54:00 +0300

    sample codeStyle Ruby

--- Code.rb
+++ Code.rb
@@ -1,15 +1,43 @@
 #refactor all
 
+class Sample
 
+  def initialize(a)
+    chislo(a)
+  end
 
-def chislo(a)
+  def chislo(a)
 
-  if a >2
-    puts "HuI!"
-  else
-    puts "Hellow Word!!!"
+    if a >2
+      puts "Hu!", "Whats My name ?"
+    else
+      puts "Hellow Word!!!"
+    end
+
+  end
+end
+
+class SampleTwo < Sample
+
+  firstName  = "Vladimir"
+  secondName = "Voronin"
+
+  def massiv(a)
+        a.each {|cell| puts cell}
   end
 
 end
 
-chislo(10)
+ST = SampleTwo.new(10)
+A = 3
+B = 5
+
+
+case
+when A == 3
+  puts "TRUE"
+when B == 5
+  puts "TRUE"
+when A < 0 ||  B >5
+    puts "FALSE"
+end
